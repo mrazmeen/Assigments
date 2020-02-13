bed3d9c58f88c34140b46bbe8607cd02868c1202
mrazmeen <razmeenrafeek@gmailcom>
2020-02-13 22:02:18 +0530

    removed

--- allocation/src/main/java/com/razmeen/allocation/repository/AllocationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.razmeen.allocation.repository;
-
-import com.razmeen.allocation.model.Allocation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AllocationRepository extends JpaRepository<Allocation,Integer> {
-}
--- microserices_allocation/src/main/java/com/razmeen/microserices2/repositories/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.razmeen.microserices2.repositories;
-
-import com.razmeen.microserices2.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Integer>{
-}
